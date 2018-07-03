a7d1343aa255739949d8d753827bb4e06cceabc2
enrique novoa <luisenriquenovoa@gmail.com>
2018-07-02 21:19:25 -0700

    Prototype + this + Recursion

--- main.js
+++ main.js
@@ -2,25 +2,103 @@ console.log("Enrique is coding.");
 
 // // HOW MANY 1s ARE IN A SORTED BINARY ARRAY.
 
-function BinarySearch(arr) {
-  var low = 0,
-      high = arr.length - 1,
-      index = -1;
+// function BinarySearch(arr) {
+//   var low = 0,
+//       high = arr.length - 1,
+//       index = -1;
+//
+//   while (low <= high) {
+//     var mid = Math.floor((high+low)/2);
+//
+//     if (arr[mid] == 1) {
+//       index = mid;
+//       high = mid - 1;
+//     } else if (arr[mid] == 0) {
+//       low = mid + 1;
+//     }
+//   }
+//
+//   return index == -1 ? "There are no 1s in the 'sorted' array." : arr.length - index;
+// }
+//
+// var numbers = [0, 0, 0, 0, 0, 0, 0, 0, 1];
+//
+// console.log(BinarySearch(numbers));
 
-  while (low <= high) {
-    var mid = Math.floor((high+low)/2);
+// function Dog() {
+// }
+// Dog.prototype.bark = function() {
+//  console.log('woof!');
+// };
+//
+// var fido = new Dog();
+// fido.bark(); // ‘woof!’
 
-    if (arr[mid] == 1) {
-      index = mid;
-      high = mid - 1;
-    } else if (arr[mid] == 0) {
-      low = mid + 1;
-    }
+
+// Car Creation
+
+// function Car() {
+//
+// }
+//
+// Car.prototype.drive = function() {
+//   console.log("Drive");
+// }
+//
+// var benz = new Car();
+//
+// benz.drive();
+
+// Rectangle Creation
+
+// function Rectangle(width, height) {
+//   this.width = width;
+//   this.height = height;
+// }
+//
+// Rectangle.prototype.area = function() {
+//   return this.width * this.height;
+// };
+//
+// var rect = new Rectangle(3, 4);
+//
+// console.log(rect.area());
+//
+// function Square(length) {
+//   this.width = this.height = length;
+// }
+//
+// Square.prototype = Object.create(Rectangle.prototype);
+//
+// var square = new Square(5);
+//
+// console.log(square.area());
+//
+// Square.prototype.diagonal = function() {
+//   return Math.sqrt(this.area() * 2);
+// }
+//
+// console.log(square.diagonal());
+
+
+// Factorial
+
+function factorial( n ) {
+  if ( n === 1 ) {
+    return 1;
   }
-
-  return index == -1 ? "There are no 1s in the 'sorted' array." : arr.length - index;
+  return n * factorial( n - 1 );
 }
 
-var numbers = [0, 0, 0, 0, 0, 0, 0, 0, 1];
+console.log(factorial(10));
 
-console.log(BinarySearch(numbers));
+// Reserving a String
+
+function reverse( str ) {
+  if ( str.length <= 1 ) {
+    return str;
+  }
+  return reverse(str.substr(1)) + str[0];
+}
+
+console.log(reverse("incondicional"));
