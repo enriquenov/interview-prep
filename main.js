420faa5aee6f3043b2d38ebfa8219a671d67ec52
enrique novoa <luisenriquenovoa@gmail.com>
2018-07-03 00:01:58 -0700

    Supermarket Queue

--- main.js
+++ main.js
@@ -105,12 +105,41 @@ console.log(BinarySearch(numbers));
 
 // Reduce
 
-var scores = [89, 76, 47, 95];
-var initialValue = 0;
-var reducer = function (accumulator, item) {
- return accumulator + item
-};
-var total = scores.reduce(reducer, initialValue);
-var average = total / scores.length;
+// var scores = [89, 76, 47, 95];
+// var initialValue = 0;
+// var reducer = function (accumulator, item) {
+//   return accumulator + item;
+// };
+// var total = scores.reduce(reducer, initialValue);
+// var average = total / scores.length;
+//
+// console.log(average);
 
-console.log(average);
+
+// The Supermarket Queue
+
+function queueTime(customers, n) {
+  if (n < 1) return 0;
+  if (n === 1) return customers.reduce((acc, customer) => acc + customer, 0);
+
+  var head = customers.slice(0, n);
+  var tail = customers.slice(n);
+
+  function reducer(acc, customer) {
+    var minIdx = acc.indexOf(Math.min(...acc));
+
+    console.log(minIdx);
+    console.log("This is the acc minIdx: " + acc[minIdx]);
+    console.log("This is the customer: " + customer);
+
+    acc[minIdx] += customer;
+
+    return acc;
+  };
+
+  var lines = tail.reduce(reducer, head);
+
+  return Math.max(...lines);
+}
+
+console.log(queueTime([10,2,6,3], 2));
